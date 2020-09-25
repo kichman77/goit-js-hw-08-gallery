1580c5bad3fcfe3b06b9a5ad75b179f1893717cc
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-25 04:21:48 +0300

    task 8 in progress

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import galleryItems from './gallery-items.js';
+import galleryItems from "./gallery-items.js";
 console.log(galleryItems);
-const list = document.querySelector('.js-gallery');
-const div = document.querySelector('.js-lightbox');
-console.log(div);
-const modalImg = document.querySelector('.lightbox__image');
-console.log(modalImg);
-const btn = document.querySelector('.lightbox__button');
-console.log(btn);
+const list = document.querySelector(".js-gallery");
+const div = document.querySelector(".js-lightbox");
+//console.log(div);
+const modalImg = document.querySelector(".lightbox__image");
+//console.log(modalImg);
+const btn = document.querySelector(".lightbox__button");
+//console.log(btn);
+const modal = document.querySelector(".lightbox__overlay");
+//console.log(modal);
 
-
-  // / <li class="gallery__item">
+// / <li class="gallery__item">
 //   <a
 //     class="gallery__link"
 //     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
@@ -23,55 +24,68 @@ console.log(btn);
 //   </a>
 // </li>
 
-galleryItems.map((item) => {
+galleryItems.map((item, i) => {
   // console.log(item);
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   // console.log(img);
-  img.classList.add('gallery__image');
+  img.classList.add("gallery__image");
   img.src = item.preview;
-  img.setAttribute('data-source', item.original);
-  img.alt = item.description
+  img.setAttribute("data-source", item.original);
+  img.alt = item.description;
+  img.setAttribute("data-index", i);
 
-  const link = document.createElement('a');
- // console.log(link);
-  link.classList.add('gallery__link');
+  const link = document.createElement("a");
+  // console.log(link);
+  link.classList.add("gallery__link");
   link.href = item.original;
 
   link.append(img);
 
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   //console.log(li);
-  li.classList.add('gallery__item');
+  li.classList.add("gallery__item");
 
   li.append(link);
 
   list.append(li);
 });
 
-
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(e.target);
   console.log(e.target.dataset.source);
   let modalLink = e.target.dataset.source;
-  div.classList.add('is-open');
+  div.classList.add("is-open");
   modalImg.src = modalLink;
 });
 
-btn.addEventListener('click', () => {
-  div.classList.remove('is-open');
-  modalImg.src = '';
+btn.addEventListener("click", () => {
+  div.classList.remove("is-open");
+  modalImg.src = "";
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+modal.addEventListener("click", () => {
+  div.classList.remove("is-open");
+  modalImg.src = "";
+});
+window.addEventListener("keyup", (event) => {
+  console.log(event.keyCode);
+  if (event.keyCode === 27) {
+    div.classList.remove("is-open");
+    modalImg.src = "";
+  }
+});
+window.addEventListener("keyup", (event) => {
+  if (event.keyCode === 37) {
+  }
+});
+window.addEventListener("keyup", (event) => {
+  event.preventDefault();
+  console.dir(event.target.childNodes[0].dataset.index);
+  let i = event.target.childNodes[0].dataset.index;
+  console.log(galleryItems[i + 1]);
+  const img = galleryItems.find((elem, idx) => (idx === i + 1 ? elem : ""));
+  console.log(img);
+  if (event.keyCode === 39) {
+  }
+});
